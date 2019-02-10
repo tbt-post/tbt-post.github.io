490453d08eb8801375428936be971b05501abbea
blackforest6230 <verechak93@gmail.com>
2019-02-10 21:01:10 +0200

    rm _site

--- _site/assets/js/tipsForApi.js
+++
@@ -1,6 +0,0 @@
-var element = document.querySelectorAll('.language-shell')
-var array = element
-array.forEach(function(element) {
-  // console.log(element);
-  element.dataset.tooltip='Нажмите для копирования';
-});
