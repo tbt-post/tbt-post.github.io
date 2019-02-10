2badca9b8f2f91fedd261988bae7b4ec8ab4d8dc
blackforest6230 <verechak93@gmail.com>
2019-02-10 20:59:49 +0200

    updated 'api' and 'home' pages

---
+++ _site/README.md
@@ -0,0 +1,1 @@
+# About page tbt-post.github.io
---
+++ _site/assets/js/tipsForApi.js
@@ -0,0 +1,6 @@
+var element = document.querySelectorAll('.language-shell')
+var array = element
+array.forEach(function(element) {
+  // console.log(element);
+  element.dataset.tooltip='Нажмите для копирования';
+});
